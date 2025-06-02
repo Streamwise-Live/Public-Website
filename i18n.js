0d3b8a9ca2cfbfae79ea1135191d9ea17592a7a0
Eduard Anton <eduard.anton@mail.mcgill.ca>
2025-06-01 20:55:49 -0400

    Updated main and join pages

--- i18n.js
+++ i18n.js
@@ -26,79 +26,149 @@ $(function () {
             resources: {
                 en: {
                     translation: {
-                        tryButton: 'Try it out for FREE',
-                        contactButton: 'Contact Us',
-                        topContent: {
-                            title: 'REAL-TIME EDITING, REAL-TIME RESULTS.',
-                            paragraph: 'The easiest way of using AI to put the fun back into content creation while protecting your work from sanctions and demonetizations on Twitch, Youtube, Instagram, and more.'
+                        nav: {
+                            featuresBtn: 'Features ▾',
+                            aboutBtn: 'About',
+                            featuresMenu: {
+                                aiOption: 'How it works',
+                                customizeOption: 'Customizability',
+                                missionOption: 'Made for you',
+                            },
+                            tryBtn: 'Join the Waitlist!'
                         },
-                        sectionDesc: {
+                        sectionHero: {
+                            title: 'WE MODERATE YOUR LIVESTREAM SO YOU DON\'T HAVE TO',
+                            description: 'Stay focused on your content while our AI moderates in real time, keeping your stream safe, compliant, and free from strikes, bans, and demonetization.'
+                        },
+                        featuresSection: {
                             title: 'EFFORTLESS HIGH QUALITY.',
-                            protection: {
+                            feature1: {
                                 title: 'Real-time protection',
-                                paragraph: 'Customize our AI models to enhance your stream\'s audio and video by filtering out unwanted content in real-time. Take back control of your livestream and avoid platform penalties.'
+                                description: 'We monitor your video and audio as you stream, with real-time latency so that any unwanted content is automatically filtered out.'
                             },
-                            possibilities: {
+                            feature2: {
                                 title: 'Unlimited possibilities',
-                                paragraph: 'Introduce unlimited profile configurations to your streams on the cloud with multistream, layout designs, statistics analysis, and more. Let your creativity run wild!'
+                                description: 'Think beyond basic filters. Our modular tools let you personalize your stream your way — with full transparency and flexibility.'
                             },
-                            integration: {
+                            feature3: {
                                 title: 'Seamless integration',
-                                paragraph: 'Seamlessly integrates with your favorite streaming tools and platforms. Never compromise on your current workflow, without exceptions.'
+                                description: 'Seamlessly integrates with your favorite streaming tools, Twitch, YouTube, and more, without disrupting your workflow. Enjoy full compatibility, multistreaming, and stream analytics.'
+                            },
+                            moreBtn: 'See LiveGuard in action'
+                        },
+                        creatorSection: {
+                            title: 'CREATOR FIRST',
+                            description1: 'Regardless of the streaming platform, we believe that you should not have to worry about unpredictable and unfair copyrighted content and ToS violations when livestreaming. Push your best content out there, and let us worry about the details.',
+                            description2: 'We use AI to understand the context within your livestream to generate desired content and edits for your stream\'s video and audio.',
+                            description3: 'We are committed to putting you at the forefront of LiveGuard\'s priorities',
+                            moreBtn: 'See how this benefits you'
+                        },
+                        trySection: {
+                            title: 'INTERESTED IN BETA TESTING?',
+                            description: 'We are always looking for content creators to help us build our platform and bring new ideas to the table!',
+                            moreBtn: 'Contact us!'
+                        },
+                        footer: {
+                            aboutSection: {
+                                title: 'About',
+                                link1: 'Our Mission',
+                                link2: 'Team',
+                                link3: 'Careers'
+                            },
+                            contactSection: {
+                                title: 'Reach us here:'
+                            },
+                            policiesSection: {
+                                title: 'Policies',
+                                link1: 'Privacy',
+                                link2: 'Terms'
+                            },
+                            helpSection: {
+                                title: 'Help',
+                                link1: 'Documentation',
+                                link2: 'Support'
                             }
                         },
-                        sectionML: {
-                            title: 'CREATOR FIRST',
-                            paragraph1: 'Regardless of the streaming platform, we believe that you should not have to worry about unpredictable and unfair copyrighted content and ToS violations when livestreaming. Push your best content out there, and let us worry about the details.',
-                            paragraph2: 'We use AI to understand the context within your livestream to generate desired content and edits for your stream\'s video and audio.'
-                        },
-                        sectionBeta: {
-                            title: 'INTERESTED IN BETA TESTING?',
-                            paragraph: 'We are always looking for content creators to help us build our platform and bring new ideas to the table!'
-                        },
-                        betaPage: {
-                            title: 'Hey! You\'re here early!',
-                            paragraph: 'Join our beta testing program to get early access to our latest features for FREE and help us innovate a life changing service for all content creators out there.',
-                            outro: 'Follow us on social media to not miss any update!'
+                        joinPage: {
+                            formSection: {
+                                title: "Hey, You're here early!",
+                                description: "Join our waitlist to get early access to our latest features for FREE and help us innovate a life changing service for all content creators out there.",
+                                followUs: "Follow us on social media"
+                            }
                         }
                     }
                 },
                 fr: {
                     translation: {
-                        tryButton: 'Essayez GRATUITEMENT',
-                        contactButton: 'Contactez-nous',
-                        topContent: {
-                            title: 'MONTAGE LIVE, RÉSULTATS INSTANTANÉS.',
-                            paragraph: 'Le moyen le plus simple d\'utiliser l\'IA pour retrouver le plaisir de la création de contenu, tout en protégeant votre travail des sanctions et de la démonétisation sur Twitch, YouTube, Instagram et plus encore.'
+                        nav: {
+                            featuresBtn: 'Fonctionnalités ▾',
+                            aboutBtn: 'À propos',
+                            featuresMenu: {
+                            aiOption: 'Comment ça marche',
+                            customizeOption: 'Personnalisation',
+                            missionOption: 'Conçu pour vous'
+                            },
+                            tryBtn: 'Rejoignez la liste d\'attente !'
                         },
-                        sectionDesc: {
-                            title: 'HAUTE QUALITÉ SANS EFFORT.',
-                            protection: {
+                        sectionHero: {
+                            title: 'NOUS MODÉRONS VOTRE LIVESTREAM POUR VOUS',
+                            description: 'Restez concentré sur votre contenu pendant que notre IA modère en temps réel, gardant votre diffusion sûre, conforme et protégée contre les avertissements, bannissements et démonétisations.'
+                        },
+                        featuresSection: {
+                            title: 'QUALITÉ SANS EFFORT.',
+                            feature1: {
                                 title: 'Protection en temps réel',
-                                paragraph: 'Personnalisez nos modèles d\'IA pour améliorer le son et la vidéo de votre stream en filtrant le contenu indésirable en temps réel. Reprenez le contrôle de votre livestream et évitez les pénalités des plateformes.'
+                                description: 'Nous surveillons votre vidéo et votre audio pendant que vous diffusez, avec une latence minimale afin que tout contenu indésirable soit automatiquement filtré.'
                             },
-                            possibilities: {
+                            feature2: {
                                 title: 'Possibilités illimitées',
-                                paragraph: 'Accédez à des options de configuration illimitées pour vos streams sur le cloud avec le multistream, la conception de mises en page, l\'analyse de statistiques et plus encore. Libérez votre créativité!'
+                                description: 'Allez au-delà des filtres de base. Nos outils modulaires vous permettent de personnaliser votre stream à votre manière — en toute transparence et flexibilité.'
                             },
-                            integration: {
-                                title: 'Intégration facile',
-                                paragraph: 'Intégration transparente avec vos outils et plateformes de streaming préférés. Ne faites jamais de compromis sur votre flux de travail actuel, sans exception.'
+                            feature3: {
+                                title: 'Intégration transparente',
+                                description: 'S\'intègre parfaitement à vos outils de streaming préférés, Twitch, YouTube, et bien d\'autres, sans perturber votre flux de travail. Profitez d\'une compatibilité totale, du multistreaming et d\'analyses de stream.'
+                            },
+                            moreBtn: 'Voir LiveGuard en action'
+                        },
+                        creatorSection: {
+                            title: 'LES CRÉATEURS AVANT TOUT',
+                            description1: 'Quelle que soit la plateforme de streaming, vous ne devriez pas avoir à vous soucier des violations de droits d\'auteur ou des conditions d\'utilisation imprévisibles et injustes. Publiez votre meilleur contenu et laissez-nous gérer les détails.',
+                            description2: 'Nous utilisons l\'IA pour comprendre le contexte de votre livestream afin de générer le contenu et les modifications souhaités pour la vidéo et l\'audio de votre stream.',
+                            description3: 'Nous nous engageons à faire de vous la priorité absolue de LiveGuard.',
+                            moreBtn: 'Découvrez ce que cela vous apporte'
+                        },
+                        trySection: {
+                            title: 'INTÉRESSÉ(E) PAR LE PROGRAMME BÊTA ?',
+                            description: 'Nous cherchons toujours des créateurs de contenu pour nous aider à construire notre plateforme et à proposer de nouvelles idées !',
+                            moreBtn: 'Contactez-nous !'
+                        },
+                        footer: {
+                            aboutSection: {
+                                title: 'À propos',
+                                link1: 'Notre mission',
+                                link2: 'Équipe',
+                                link3: 'Carrières'
+                            },
+                            contactSection: {
+                                title: 'Nous contacter :'
+                            },
+                            policiesSection: {
+                                title: 'Politiques',
+                                link1: 'Confidentialité',
+                                link2: 'Conditions'
+                            },
+                            helpSection: {
+                                title: 'Aide',
+                                link1: 'Documentation',
+                                link2: 'Support'
                             }
                         },
-                        sectionML: {
-                            title: 'LES CREATEURS D\'ABORD',
-                            paragraph1: 'Quelle que soit la plateforme de streaming, vous ne devriez pas avoir à vous soucier des violations de droits d\'auteur et des conditions d\'utilisation imprévisibles et injustes lors du streaming en direct. Diffusez votre meilleur contenu et laissez-nous nous occuper des détails.',
-                            paragraph2: 'Nous utilisons l\'IA pour comprendre le contexte de votre livestream afin de générer le contenu et les modifications souhaités pour la vidéo et l\'audio de votre stream.'
-                        },
-                        sectionBeta: {
-                            title: 'INTÉRESSÉ(E) PAR LE PROGRAMME BÊTA?',
-                            paragraph: 'Nous sommes toujours à la recherche de créateurs de contenu pour nous aider à développer notre plateforme et apporter de nouvelles idées!'
-                        },
-                        betaPage: {
-                            title: 'Hé! Tu es en avance!',
-                            paragraph: 'Rejoignez notre programme de test bêta pour accéder gratuitement à nos dernières fonctionnalités en avant-première et aidez-nous à innover pour créer un service qui va changer le monde pour tous les créateurs de contenu.',
-                            outro: 'Suivez-nous sur les réseaux sociaux pour ne manquer aucune mise à jour!'
+                        joinPage: {
+                            formSection: {
+                                title: "Hé! Tu es en avance!",
+                                description: "Rejoignez note liste d'attente pour accéder gratuitement à nos dernières fonctionnalités en avant-première et aidez-nous à innover pour créer un service qui va changer le monde pour tous les créateurs de contenu.",
+                                followUs: "Suivez-nous sur les réseaux sociaux pour ne manquer aucune mise à jour!"
+                            }
                         }
                     }
                 }
@@ -112,17 +182,21 @@ $(function () {
       
             // fill language switcher
             Object.keys(languages).map((lng) => {
-                const opt = new Option(languages[lng].nativeName, lng);
-                if (lng === i18next.resolvedLanguage) {
-                    opt.setAttribute("selected", "selected");
-                }
-                $('#languageSwitcher').append(opt);
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+
+                a.href = "#";
+                a.setAttribute("data-lang", lng);
+                a.textContent = languages[lng].nativeName;
+                li.appendChild(a);
+                $('#languageSwitcher').append(li);
             });
-            $('#languageSwitcher').change((a, b, c) => {
-                const chosenLng = $(this).find("option:selected").attr('value');
+            $('#languageSwitcher').on('click', 'a[data-lang]', function (e) {
+                const chosenLng = $(this).data('lang');
                 i18next.changeLanguage(chosenLng, () => {
                     rerender();
                 });
+                $('#languageSwitcher').removeClass('show');
             });
     
             rerender();
